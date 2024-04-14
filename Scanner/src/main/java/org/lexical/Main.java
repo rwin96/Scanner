839aeda20b0e48925d2df9a11adefe7a3d181e3b
rwin96 <134379705+rwin96@users.noreply.github.com>
2024-04-14 15:41:50 +0330

    Some change in txt file and main & add id column in db

--- Scanner/src/main/java/org/lexical/Main.java
+++ Scanner/src/main/java/org/lexical/Main.java
@@ -1,9 +1,18 @@
 package org.lexical;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         String inputFileName = "input.txt";
         TextFileReader fileReader = new TextFileReader(inputFileName);
-        LexicalAnalyzer.scan(fileReader.getLinesList());
+        List<Token> tokenList = LexicalAnalyzer.scan(fileReader.getLinesList());
+
+        for(Token tk : tokenList) {
+            System.out.format("%-50s\t Scope: %s\n", tk.toString(), tk.getScope());
+        }
+        System.out.println(tokenList.size());
+
+        TokenCreator.updateSavedTokensDB(tokenList);
     }
 }
